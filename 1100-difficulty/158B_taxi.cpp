cd77cb02e6c79326caaec49a5b0fcf0417cb4dbd
arthurvergacas <arthurvdm@gmail.com>
2021-07-22 16:11:21 -0300

    Add problem 158B

--- 1100-difficulty/158B_taxi.cpp
+++ 1100-difficulty/158B_taxi.cpp
@@ -9,5 +9,35 @@ int main() {
   int numGroups;
   cin >> numGroups;
 
-   return 0;
+  vector<int> groups(numGroups);
+
+  for (int i = 0; i < numGroups; i++) {
+    int a;
+    cin >> a;
+    groups.push_back(a);
+  }
+
+  sort(groups.begin(), groups.end(), greater<int>());
+
+  int numTaxis = 0;
+
+  for (int i = 0; i < numGroups; i++) {
+
+    int sum = groups[i] + groups[numGroups - 1];
+    if (sum <= 4) {
+      numTaxis++;
+      numGroups--;
+
+      while (sum + groups[numGroups - 1] <= 4) {
+        sum += groups[numGroups - 1];
+        numGroups--;
+      }
+    } else {
+      numTaxis++;
+    }
+  }
+
+  cout << numTaxis << "\n";
+
+  return 0;
 }
